94b39f19053b2be904e3889dda9831dc48275099
Mohit Kumar <mohitkumarsolanki25112005@gmail.com>
2025-08-19 08:43:05 +0530

    [mohitkumar.java] question 1

---
+++ assignment-1/assignmentDate_19Ques1.java
@@ -0,0 +1,20 @@
+class Car {
+    String model;
+    int year;
+}
+
+public class assignmentDate_19Ques1 {
+    public static void main(String[] args) {
+        Car c1 = new Car();
+        c1.model = "Toyota";
+        c1.year = 2020;
+
+        Car c2 = new Car();
+        c2.model = "Honda";
+        c2.year = 2022;
+
+        System.out.println(c1.model + " - " + c1.year);
+        System.out.println(c2.model + " - " + c2.year);
+    }
+    
+}
