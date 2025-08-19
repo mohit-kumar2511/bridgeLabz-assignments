8f7e6c5290b27fbfddacb00866cbf77ff62fdcd1
Mohit Kumar <mohitkumarsolanki25112005@gmail.com>
2025-08-19 10:22:30 +0530

    [mohitkumar.java]Assignmentquestion5 done

--- assignment-1/assignmentDate_19Ques4.java
+++ assignment-1/assignmentDate_19Ques4.java
@@ -10,7 +10,7 @@ class Student {
 public class assignmentDate_19Ques4 {
     public static void main(String[] args) {
         Student s1 = new Student("Mohit", 90);
-        Student s2 = new Student("Amit", 85);
+        Student s2 = new Student("Vishal", 85);
 
         System.out.println(s1.name + " - " + s1.marks);
         System.out.println(s2.name + " - " + s2.marks);
--- assignment-1/assignmentDate_19Ques5.java
+++ assignment-1/assignmentDate_19Ques5.java
@@ -1,3 +1,12 @@
+class Test {
+    static int count = 0;
+}
 public class assignmentDate_19Ques5 {
-    
+    public static void main(String[] args) {
+        Test t1 = new Test();
+        t1.count = 5; 
+
+        Test t2 = new Test();
+        System.out.println("Access from t2: " + t2.count); 
+    }
 }
